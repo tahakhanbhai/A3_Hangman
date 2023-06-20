6340f5688d99b90046e97e95620ab86eb787aca6
tahakhanbhai <taha.khanbhai@mail.utoronto.ca>
2023-06-20 13:50:41 -0400

    Fixed mistake counter, updated gameplay comments

--- TAHA_A3.R
+++ TAHA_A3.R
@@ -4,26 +4,33 @@
 #User input can't have multiple characters, non-letter characters, numbers, or already have been guessed before.
 check_user_input <- function(user_guess, guessed_letters) {
   if (nchar(user_guess) != 1 || user_guess %in% guessed_letters || !(grepl("^[a-zA-Z]$", user_guess))) {
-    print("Invalid input: Please input a single letter or guess the entire word.")
+    print("Invalid input: Please input a single letter that has not been guessed before.")
     return(FALSE)
   }
   return(TRUE)
 }
 
 #Creating a function for the visual display given to users so that they know which letters they have guessed in the word, and in which order.
-visual_display <- function(mystery_word, guessed_letters) {
-  word_display <- ""
+visual_display <- function(mystery_word, user_guess) {
+  word.length <- length(strsplit(mystery_word, "")[[1]])
+  word_display <- rep('_', word.length)
 
-  for (letter in mystery_word) {
-    if (letter %in% guessed_letters) {
-      word_display <- paste(word_display, letter, sep = "")
-    } else {
-      word_display <- paste(word_display, "_", sep = "")
+  for (i in 1:word.length){
+    if (mystery_word[i] == user_guess) {
+      word_display[i] <- user_guess
     }
   }
-  cat("Current state: ", paste(strsplit(word_display, "")[[1]], collapse = " "), "\n")
+  return(paste(visual, collapse = ""))
 }
 
+  #    word_display <- paste(word_display, letter, sep = "")
+ #   } else {
+#      word_display <- paste(word_display, "_", sep = "")
+#    }
+ # }
+  #cat(word_display, "\n")
+  #return(word_display)
+#}
 
 
 #Prepare a dictionary of words to choose from and save it in a txt file (one column) and save it in the project directory
@@ -32,15 +39,17 @@ visual_display <- function(mystery_word, guessed_letters) {
 play_game <- function() {
 
 wordlist <- readLines("Hangman_Words.txt")
-mystery_word <- sample(wordlist, 1)
+mystery_word <- sample(wordlist, 1, prob = NULL)
 mistake <- 0
 guessed_letters <- c()
 #Inform the user on the length of the secret word. Hint: You may test nchar()
 
 #Inform the user about the number of wrong guesses/tries allowed. You decide on the rule here and implement it. Please comment your code appropriately.
 print("Welcome to Hangman!")
+print("The objective of the game is to guess the mystery word, one letter at a time")
+print("You are allowed 5 wrong guesses before the game is over. You can also guess the mystery word in full after every turn")
+print("Good luck!")
 print(paste0("The length of the mystery word is: ", nchar(mystery_word), " letters long."))
-print("You are allowed 5 wrong guesses before the game is over. Good luck!")
 
 
 
@@ -58,32 +67,32 @@ repeat {
     } else {
       print(paste0(user_guess, " is not in the mystery word"))
       mistake <- mistake + 1
+      print(paste0("You have ", 5-mistake, " mistakes left."))
     }
-  }
-  #FUNCTION THAT UPDATES THE DISPLAY OF THE WORD
-
-    choice <- readline(prompt = "Would you like to guess the mystery word? (Y/N): ")
-    if (choice == "N") {
-      next
-    } else {
-    guess_the_word <- toupper(readline(prompt = "Enter guess: ")) }
-    if (guess_the_word == mystery_word) {
-      print("Congrats")
-      break
-    }
-
-
-
-
     if (mistake >= 5) {
       print("No more remaining guesses. Game Over :(")
       print(paste0("The mystery word was: ", mystery_word))
       break
     }
   }
+  #FUNCTION THAT UPDATES THE DISPLAY OF THE WORD
+
+    choice <- toupper(readline(prompt = "Would you like to guess the mystery word? (Y/N): "))
+    if (choice == "N") {
+      next
+    } else {
+    guess_the_word <- toupper(readline(prompt = "Enter guess: ")) }
+    if (guess_the_word == mystery_word) {
+      print("Congratulations! You guessed the mystery word.")
+      break
+    } else {
+      print(paste0("Sorry ", guess_the_word, " is not the mystery word."))
+      print(paste0("You have ", 5-mistake, " mistakes left."))
+    }
+
+
+  }
 }
-
-
 #Check to see if the user input is in the secret word.
 #If yes, notify user and ask for next letter
 #If not, notify user.
@@ -93,7 +102,7 @@ repeat {
 #Always notify the user about the correct letters/wrong letters, remaining tries.
 
 
-}
+
 #Additional functionality you may want to consider:
 #
 #  Provide the user with a visual clue of how they are progressing. E.g. if the secret word is “correct” and the user input is ‘c’, you may want to provide them with something like this:
