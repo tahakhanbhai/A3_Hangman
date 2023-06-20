a726c4ac13f8b67be05163ec68ae811e1832c7e5
tahakhanbhai <taha.khanbhai@mail.utoronto.ca>
2023-06-20 00:37:37 -0400

    Added in main functionality of game

--- TAHA_A3.R
+++ TAHA_A3.R
@@ -29,27 +29,71 @@ visual_display <- function(mystery_word, guessed_letters) {
 #Prepare a dictionary of words to choose from and save it in a txt file (one column) and save it in the project directory
 #Read the word list from your program.
 #Choose a random element from the list. Hint: You may want to check sample() and sample.int()  functions. There are many different ways of doing this. You are welcome to experiment.
-wordlist <- read.csv("Hangman_Words.txt")
+play_game <- function() {
+
+wordlist <- readLines("Hangman_Words.txt")
 mystery_word <- sample(wordlist, 1)
+mistake <- 0
+guessed_letters <- c()
 #Inform the user on the length of the secret word. Hint: You may test nchar()
 
 #Inform the user about the number of wrong guesses/tries allowed. You decide on the rule here and implement it. Please comment your code appropriately.
 print("Welcome to Hangman!")
-print(paste0("The length of the secret word is: ", nchar(mystery_word), " letters long."))
+print(paste0("The length of the mystery word is: ", nchar(mystery_word), " letters long."))
 print("You are allowed 5 wrong guesses before the game is over. Good luck!")
 
-#Ask for user input. The user is expected to enter one character only, check for this.
 
-user_guess <- readline(prompt = " Please enter a single letter: ")
-#Optional; check if the character is a letter
-#Optional: make sure that both lower and upper case letters are acceptable
-#heck to see if the user input is in the secret word.
+
+#Ask for user input. The user is expected to enter one character only, check for this.
+repeat {
+  user_guess <- toupper(readline(prompt = "Please enter a single letter: "))
+
+  if (check_user_input(user_guess, guessed_letters) == FALSE) {
+    next
+  } else {
+    guessed_letters <- c(guessed_letters, user_guess)
+
+    if (user_guess %in% strsplit(mystery_word, "")[[1]]) {
+      print(paste0(user_guess, " is in the mystery word"))
+    } else {
+      print(paste0(user_guess, " is not in the mystery word"))
+      mistake <- mistake + 1
+    }
+  }
+  #FUNCTION THAT UPDATES THE DISPLAY OF THE WORD
+
+    choice <- readline(prompt = "Would you like to guess the mystery word? (Y/N): ")
+    if (choice == "N") {
+      next
+    } else {
+    guess_the_word <- toupper(readline(prompt = "Enter guess: ")) }
+    if (guess_the_word == mystery_word) {
+      print("Congrats")
+      break
+    }
+
+
+
+
+    if (mistake >= 5) {
+      print("No more remaining guesses. Game Over :(")
+      print(paste0("The mystery word was: ", mystery_word))
+      break
+    }
+  }
+}
+
+
+#Check to see if the user input is in the secret word.
 #If yes, notify user and ask for next letter
 #If not, notify user.
 #If user has not exhausted the available tries, ask for the next letter.
 #If tries are exhausted, notify user that they’ve lost. Reveal secret and exit.
 #Make sure that your code never goes into an infinite loop, and always manages to exit gracefully (ie without throwing an error)
 #Always notify the user about the correct letters/wrong letters, remaining tries.
+
+
+}
 #Additional functionality you may want to consider:
 #
 #  Provide the user with a visual clue of how they are progressing. E.g. if the secret word is “correct” and the user input is ‘c’, you may want to provide them with something like this:
